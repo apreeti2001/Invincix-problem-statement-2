1b0b8806407dc2587ea5d80be83a620dd8ed6b75
apreeti2001 <96828367+apreeti2001@users.noreply.github.com>
2022-02-04 16:45:24 +0530

    Add files via upload

---
+++ index.py
@@ -0,0 +1,62 @@
+# Python program to find Index
+# of B to be replaced with A to get
+# longest continuous sequence
+# of As in a binary array
+
+# Returns index of B to be
+# replaced with A to get longest
+# continuous sequence of As.
+# If there is no B in array, then
+# it returns -1.
+def max_A_index(arr,n):
+	
+	# for maximum number of A around B
+	max_count = 0
+
+	# for storing result
+	max_index =0
+
+	# index of previous B
+	prev_b = -1
+
+	# index of previous to previous B
+	prev_prev_b = -1
+
+	# Traverse the input array
+	for items in range(n):
+	
+		# If current element is B,
+		# then calculate the difference
+		# between items and prev_prev_b
+		if (arr[items] == 'B'):
+		
+			# Update result if count of
+			# 1s around prev_b is more
+			if (items - prev_prev_b > max_count):
+			
+				max_count = items - prev_prev_b
+				max_index = prev_b
+			
+
+			# Update for next iteration
+			prev_prev_b = prev_b
+			prev_b = items
+
+	# Check for the last encountered B
+	if (n-prev_prev_b > max_count):
+		max_index = prev_b
+
+	return max_index
+
+# Driver program
+
+arr = ['A','A','B','B','A','B','A','A','A','B','A','A','A','A']
+n = len(arr)
+print("The string is : ")
+for i in arr :
+    print(i ,end = " ")
+
+print("\n\nIndex of B to be replaced at is ",
+	max_A_index(arr, n))
+
+
